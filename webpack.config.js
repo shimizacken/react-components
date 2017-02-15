29f52d2ff6f71398dee97c06d3c2439cf04a0699
Shimi Zacken <shimizacken@gmail.com>
2017-02-15 20:43:03 +0200

    webpack configured

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,24 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-
     entry: './app/app.jsx',
     output: {
-        path: path.resoplve
-    }
-
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+     plugins: [
+        //new webpack.optimize.CommonsChunkPlugin({ name: "vendor", filename: "vendor.bundle.js" }),
+        //new HappyPack({ loaders: ['babel-loader?presets[]=es2015']}),
+    ],
+     module: {
+         loaders: [
+             {
+                 test: /\.js$/,
+                 loader: 'babel-loader',
+                 query: {
+                     presets: ['es2015']
+                 }
+             }
+         ]
+     },
 };
